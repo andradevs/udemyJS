7c4c9a9f5ea442f82db8ca2b43cd29254cff728b
Kerrisson <guiguk9@gmail.com>
2019-05-25 15:07:41 -0300

    até a aula 100

--- objeto/OO.js
+++ objeto/OO.js
@@ -2,3 +2,20 @@
 
 //Procedural
 
+processamento(valor1,valor2,valor3)
+
+//OO
+objeto ={
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+
+    }
+}
+
+objeto.processamento() //foco no Objeto
+
+// principios importantes:
+// 1.abstração
+// 2. encapsulamento
