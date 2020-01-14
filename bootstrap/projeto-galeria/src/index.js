2ec6af46c675c3699770a49385e6608f94275d10
andradevs <guilhermeandradevs@gmail.com>
2020-01-14 00:23:23 -0300

    Fim do modulo de projeto(jQuery/bootstrap/webpack)

---
+++ bootstrap/projeto-galeria/src/index.js
@@ -0,0 +1,12 @@
+// Arquivo SASS
+
+import './scss/index.scss'
+
+//Dependencias
+import 'jquery'
+import 'bootstrap'
+
+// Meus arquivos Js
+
+import  './js/core/includes'
+import './js/plugins/cityButtons'
